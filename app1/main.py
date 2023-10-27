f0e43f50a2f7e94597f8340634c5ae4117f44059
Reji Raveendran Nair <rejinairgcp@gmail.com>
2023-10-27 21:20:45 +0100

    Initial Commit

---
+++ app1/main.py
@@ -0,0 +1,5 @@
+# print() function takes input from the user and prints it to the screen
+
+# input() function takes input from the user and returns it as a string.
+user_text = input("Enter a todo:")
+print(user_text)
