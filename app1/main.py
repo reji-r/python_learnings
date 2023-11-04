15c444974db4be2215e45958bc87863beac893c3
Reji Raveendran Nair <rejinairgcp@gmail.com>
2023-11-04 20:24:19 +0000

    Pushing some changes

--- app1/main.py
+++ app1/main.py
@@ -1,5 +1,13 @@
 # print() function takes input from the user and prints it to the screen
-
 # input() function takes input from the user and returns it as a string.
-user_text = input("Enter a todo:")
-print(user_text)
+# type() function takes input from the user and returns data type as a string.
+user_input = "Enter a todo: "
+print("Type of user input: ", type(user_input))
+# Multiple todos
+todo1 = input(user_input)
+todo2 = input(user_input)
+todo3 = input(user_input)
+
+todos = [todo1, todo2, todo3, "Hello"]
+print(todos)
+print("Type of todos: ", type(todos))
